a5db6acfe06e2f0fb8b34d0ffff393145356e17f
jorian101 <whaam202101@gmail.com>
2024-05-10 09:43:52 -0400

    primer componente

---
+++ 2Parcial/src/components/Image.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Image = () => {
+  return (
+    <div className='img-container'>
+        <div>
+         <img src='/.assets/gym.png'></img>
+        </div>
+        <div>
+            <img src='/.icono.png'></img>
+        </div>
+    </div>
+  )
+}
+
+export default Image
