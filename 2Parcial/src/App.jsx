08bd1a2026b4c8e3dc2ee5a9eeead03f76ed6265
jorian101 <whaam202101@gmail.com>
2024-05-10 09:53:00 -0400

    composicion de componente

--- 2Parcial/src/App.jsx
+++ 2Parcial/src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import './App.css'
 const App = () => {
   return (
-    <Navigation/>
+    <div>
+       <Image/>
+        <Content/>
+    </div>
   )
 }
 
---
+++ 2Parcial/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Content from './Content'
+import Image from './Image'
+const Card = () => {
+  return (
+    <div className="card" id="card">
+        <Image/>
+        <Content/>
+    </div>
+
+  )
+}
+
+export default Card
