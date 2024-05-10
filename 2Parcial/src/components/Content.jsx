494d2828b9313fe6f07074cd839b3063fdb76a0c
jorian101 <whaam202101@gmail.com>
2024-05-10 09:44:40 -0400

    segundo componente

---
+++ 2Parcial/src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='section'>
+        <h2>Terapia 2</h2>
+        <h3>Ejercicios pelvicos</h3>
+    </div>
+  )
+}
+
+export default Content
